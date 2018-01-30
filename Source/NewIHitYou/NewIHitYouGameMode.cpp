d62d32a4f0fe4b1810e9ea8d40faf6d0c4cdcee2
YunshanJiang <yunshantree@163.com>
2018-01-30 12:14:53 -0500

    1

--- Source/NewIHitYou/NewIHitYouGameMode.cpp
+++ Source/NewIHitYou/NewIHitYouGameMode.cpp
@@ -7,7 +7,7 @@
 ANewIHitYouGameMode::ANewIHitYouGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/SideScrollerCPP/Blueprints/SideScrollerCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/SideScrollerCPP/Blueprints/Player"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
